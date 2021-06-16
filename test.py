903143aef44527fe767ea7e1e2d5361260680bfc
Nick Fox-Gieg <nick@fox-gieg.com>
2021-06-16 14:49:46 -0400

    first test works

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import sys
 
 from latk.latk import *
-import binvox_rw.binvox_rw
+from binvox_rw.binvox_rw import *
 
 argv = sys.argv
 argv = argv[argv.index("--") + 1:] # get all args after "--"
@@ -11,7 +11,8 @@ outputDir = argv[1]
 
 def main():
     with open('chair.binvox', 'rb') as f:
-        m1 = binvox_rw.read_as_3d_array(f)
+        m1 = read_as_3d_array(f)
+        print(m1.data)
     #data = Latk(inputDir)
     #data.clean()
     #data.write(outputDir)
