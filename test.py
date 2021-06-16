11d3085eb361f2c3feda7c162e8327c5a82206b5
Nick Fox-Gieg <nick@fox-gieg.com>
2021-06-16 14:42:01 -0400

    remove submodule

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import sys
 
 from latk.latk import *
-from binvox_rw.binvox_rw import *
+import binvox_rw.binvox_rw
 
 argv = sys.argv
 argv = argv[argv.index("--") + 1:] # get all args after "--"
@@ -10,9 +10,11 @@ inputDir = argv[0]
 outputDir = argv[1]
 
 def main():
-    data = Latk(inputDir)
-    data.clean()
-    data.write(outputDir)
+    model = binvox_rw.read_binvox('chair.binvox')
+    model.voxels
+    #data = Latk(inputDir)
+    #data.clean()
+    #data.write(outputDir)
 
 print("Reading from : " + inputDir)
 print("Writing to: " + outputDir)
