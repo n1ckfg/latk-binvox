7c6e66c9f5b89e9e5d88f0ffdd43ad04ea178b0b
Nick Fox-Gieg <nick@fox-gieg.com>
2021-06-25 03:53:38 -0400

    Update test.py

--- test.py
+++ test.py
@@ -10,21 +10,27 @@ inputDir = argv[0]
 outputDir = argv[1]
 
 dim = 128
-drawReps = dim * dim 
+drawReps = dim #* dim 
 
-def lerp(x1, x2, y1, y2, x):
-    return ((y2 - y1) * x + x2 * y1 - x1 * y2) / (x2 - x1)
+def lerp(a, b, f): 
+    return (a * (1.0 - f)) + (b * f)
 
-def drawLine(x1, y1, z1, x2, y2, z2):
+def lerp3d(a, b, f):
+    x = (a[0] * (1.0 - f)) + (b[0] * f)   
+    y = (a[1] * (1.0 - f)) + (b[1] * f)   
+    z = (a[2] * (1.0 - f)) + (b[2] * f)   
+    return (x, y, z)
+
+def drawLine(data, dims, x1, y1, z1, x2, y2, z2):
     p1 = (x1, y1, z1)
     p2 = (x2, y2, z2)
     for i in range(0, drawReps):
         val = float(i) / float(drawReps) 
-        p3 = p1.lerp(p2, val)
-        x = int(p3[0])
-        y = int(p3[1])
-        z = int(p3[2])
-        voxelOn(x, y, z)
+        p3 = lerp3d(p1, p2, val)
+        x = int(p3[0] * (dims[0]-1))
+        y = int(p3[1] * (dims[1]-1))
+        z = int(p3[2] * (dims[2]-1))
+        data[x][y][z] = True
 
 def main():
     la = Latk(inputDir)
@@ -53,11 +59,17 @@ def main():
     for layer in la.layers:
         for frame in layer.frames:
             for stroke in frame.strokes:
+                for i in range(1, len(stroke.points)):
+                    p1 = stroke.points[i].co
+                    p2 = stroke.points[i-1].co
+                    drawLine(data, dims, p1[0], p1[1], p1[2], p2[0], p2[1], p2[2])
+                '''
                 for point in stroke.points:
                     x = int(point.co[0] * (dims[0]-1))
                     y = int(point.co[1] * (dims[1]-1))
                     z = int(point.co[2] * (dims[2]-1))
                     data[x][y][z] = True
+                '''
 
     with open(outputDir, 'wb') as f:
         bv.write(f)
