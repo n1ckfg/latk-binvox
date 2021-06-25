8729cceb5ae0cc0899cf31c413959bb6cb933da5
Nick Fox-Gieg <nick@fox-gieg.com>
2021-06-25 03:33:22 -0400

    Update test.py

--- test.py
+++ test.py
@@ -9,13 +9,29 @@ argv = argv[argv.index("--") + 1:] # get all args after "--"
 inputDir = argv[0]
 outputDir = argv[1]
 
+dim = 128
+drawReps = dim * dim 
+
+def lerp(x1, x2, y1, y2, x):
+    return ((y2 - y1) * x + x2 * y1 - x1 * y2) / (x2 - x1)
+
+def drawLine(x1, y1, z1, x2, y2, z2):
+    p1 = (x1, y1, z1)
+    p2 = (x2, y2, z2)
+    for i in range(0, drawReps):
+        val = float(i) / float(drawReps) 
+        p3 = p1.lerp(p2, val)
+        x = int(p3[0])
+        y = int(p3[1])
+        z = int(p3[2])
+        voxelOn(x, y, z)
+
 def main():
     la = Latk(inputDir)
     la.clean()
     la.normalize()
     #la.write(outputDir)
 
-    dim = 128
     dims = (dim, dim, dim)
     data = np.zeros((dims[0], dims[1], dims[2]), dtype=bool)
     translate = (0, 0, 0)
